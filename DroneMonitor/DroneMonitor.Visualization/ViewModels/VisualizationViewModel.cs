cd017a09536a684dac7c5e1ea4402e3a63159878
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2020-11-27 01:22:45 +0100

    started migrating existing routines

--- DroneMonitor/DroneMonitor.Visualization/ViewModels/VisualizationViewModel.cs
+++ DroneMonitor/DroneMonitor.Visualization/ViewModels/VisualizationViewModel.cs
@@ -1,4 +1,12 @@
-﻿namespace DroneMonitor.Visualization.ViewModels {
-    public class VisualizationViewModel {
+﻿using DroneMonitor.Infrastructure.Base;
+using System.IO.Ports;
+
+namespace DroneMonitor.Visualization.ViewModels {
+    public class VisualizationViewModel : ViewModelBase {
+        public VisualizationViewModel() {
+             Ports = SerialPort.GetPortNames();
+        }
+
+        public string[] Ports { get; set; }
     }
 }
